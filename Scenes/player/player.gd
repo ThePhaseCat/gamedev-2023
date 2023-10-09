f6564167e9a5259093f075623b2712bd653eb0a1
ThePhaseCat <alexmgamer10@gmail.com>
2023-10-09 08:51:27 -0600

    death zone (and player movement tweaks)

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 
 @export var SPEED = 300.0
-#no need for this - @export var JUMP_VELOCITY = -400.0
 
 @export var jump_height : float
 @export var jump_time_to_peak : float
@@ -12,8 +11,11 @@ extends CharacterBody2D
 @onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
 @onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
+#other vars
+@onready var area2D = $PlayerArea2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
@@ -40,3 +42,10 @@ func get_gravity() -> float: #returns the gravity, which is a float value
 
 func slow_friction(d):
 	velocity.x -= .02 * d
+
+
+#handle when stuff goes into player
+func _on_player_area_2d_area_entered(area):
+	var name = area.get_name()
+	if(name == "DeathZone"):
+		get_tree().reload_current_scene()
