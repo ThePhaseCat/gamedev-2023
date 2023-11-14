00755082036e5af3082a5f048fccc50994d1c2db
ThePhaseCat <alexmgamer10@gmail.com>
2023-11-13 17:57:40 -0700

    advanced collision and attack setup

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -24,6 +24,8 @@ var was_wall_normal = Vector2.ZERO
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var wall_jump_timer = $WallJumpTimer
 
+@onready var animation =$AnimationPlayer
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -57,6 +59,11 @@ func _physics_process(delta):
 	var just_left_wall = was_on_wall and not is_on_wall()
 	if just_left_wall:
 		wall_jump_timer.start()
+
+func _unhandled_input(event):
+	if(Input.is_action_just_pressed("attack")):
+		if(checks()): #must return true
+			print("can attack")
 
 func handle_gravity(delta):
 	if not is_on_floor():
@@ -112,13 +119,17 @@ func apply_air_resistance(input_axis, delta):
 func enemyJump():
 	velocity.y = jump_velocity
 
+func checks():
+	if(is_on_floor()):
+		return true
+	else:
+		return false
+
 #handle when stuff goes into player
 func _on_player_area_2d_area_entered(area):
 	var name = area.get_name()
 	if(name == "DeathZone"):
 		get_tree().reload_current_scene()
-
-
 
 func _on_player_area_2d_body_entered(body):
 	var name = body.get_name()
