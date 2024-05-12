4310add30bb00ad2a579aae63fcdb04184be256b
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-12 14:55:39 -0600

    fixed cutscene animation bugs

---
+++ Scenes/cutscenes/fox_animated_sprite.gd
@@ -0,0 +1,23 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func playIdle():
+	play("idle")
+
+func playWalk():
+	play("walk")
+
+func playJump():
+	play("jump")
+
+func playAttack():
+	play("scratch")
