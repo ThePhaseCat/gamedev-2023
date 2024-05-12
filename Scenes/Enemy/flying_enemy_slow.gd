387c9463a8b49bb105a5375f0d303e1a12af8779
Test <143560445+mjshapiro-j@users.noreply.github.com>
2024-05-12 10:50:06 -0600

    Quality of Play Fixes
    After using my sister as a beta-tester, I made the following changes:
    -added checkpoints to levels 2 and 4
    -fixed combat error with main enemy 3 in level 3
    -changed level 4 flying enemy 3 and surrounding areas to make it possible to pass
    
    She also suggested a way to tell when an enemy is nearby before they are visible and an equivalent to the minecraft shift

---
+++ Scenes/Enemy/flying_enemy_slow.gd
@@ -0,0 +1,94 @@
+extends CharacterBody2D
+
+var hp = 20
+
+var speed = 15
+var gravity = 0
+
+var is_moving_left = true
+
+@onready var ray = $RayCast2D
+@onready var sprite = $AnimatedSprite2D
+
+var attackNodeInArea = false
+var attackingNode = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprite.play("default")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(not is_on_floor()):
+		velocity.y += gravity * delta
+	
+	if(is_moving_left == true):
+		velocity.x = speed
+	else:
+		velocity.x = -speed
+	#velocity.x = speed if is_moving_left else -speed
+	detect_turn_around()
+	
+	if(global.pauseOn == false):
+		move_and_slide()
+	
+	if(hp <= 0):
+		death()
+	
+	if(attackNodeInArea == true):
+		if(attackingNode.attacking == true):
+			#print("HP: " + str(hp))
+			hp = hp - 1
+			await get_tree().create_timer(0.5).timeout
+
+func wait(duration):
+	await get_tree().create_timer(duration,false,false,true).timeout
+
+func detect_turn_around():
+	if(ray.is_colliding()):
+		if(ray.get_collider().get_name() == "player"):
+			return #do nothing because player
+		else:
+			#print("hi!")
+			is_moving_left = !is_moving_left
+			scale.x = -scale.x
+
+func _on_area_2d_body_entered(body):
+	var name = body.get_name()
+	if(name == "player"):
+		body.enemyJump()
+		death()
+
+func _on_attack_check_area_entered(area):
+	attackNodeInArea = true
+	attackingNode = area
+	hp = hp - 1
+	if(hp == 0):
+		death()
+
+func death():
+	DeathSoundManager.play()
+	queue_free()
+
+func healthDecreaseFromProjectile():
+	hp = hp - 5
+	#print("hp + " + str(hp))
+
+func _on_attack_check_area_exited(area):
+	attackNodeInArea = false
+	attackingNode = null
+
+func _on_attack_check_body_entered(body):
+	var name = body.get_name()
+	#print(name)
+	if(name == "Projectile1"):
+		hp = hp - 10
+		#print("HP: " + str(hp))
+		body.timeToDeath()
+
+
+func _on_player_check_body_entered(body):
+	var name = body.get_name()
+	#print(name)
+	if(name == "player"):
+		body.actualDeath()
